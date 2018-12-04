f0aaa5c36a16d0ac59c83e25e9e06a0de1d842a8
renzuoli <xxxxxxxxxxxxxx>
2018-12-04 16:50:35 +0800

    m

--- src/main/java/io/netty/example/echo/test/ChannelInboundHandlerAdapter3.java
+++ src/main/java/io/netty/example/echo/test/ChannelInboundHandlerAdapter3.java
@@ -1,9 +1,6 @@
 package io.netty.example.echo.test;
 
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.*;
 
 
 public class ChannelInboundHandlerAdapter3 extends ChannelInboundHandlerAdapter {
@@ -11,8 +8,9 @@ public class ChannelInboundHandlerAdapter3 extends ChannelInboundHandlerAdapter 
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("ChannelInboundHandlerAdapter3");
 
-        ChannelFuture future = ctx.channel().writeAndFlush("hello");
 
+       /* //这段代码 怎么执行都是继续执行，不阻塞 最后，ractor线程里面都是同步的
+        ChannelFuture future = ctx.channel().writeAndFlush("hello");
         future.addListener(new ChannelFutureListener() {
             public void operationComplete(ChannelFuture future) throws Exception {
                 System.out.println("======");
@@ -23,7 +21,9 @@ public class ChannelInboundHandlerAdapter3 extends ChannelInboundHandlerAdapter 
             }
         });
 
-        System.out.println("继续执行，不阻塞");
+        System.out.println("继续执行，不阻塞");*/
+
+        new Thread(new ThreadTep(ctx.channel())).start();
 
         super.channelRead(ctx, msg);
     }
@@ -47,5 +47,32 @@ public class ChannelInboundHandlerAdapter3 extends ChannelInboundHandlerAdapter 
         //ctx.pipeline().fireExceptionCaught(cause);
 
         ctx.fireExceptionCaught(cause);
+    }
+
+
+    class ThreadTep implements Runnable{
+        private Channel channel;
+
+        public ThreadTep() {
+        }
+
+        public ThreadTep(Channel channel) {
+            this.channel = channel;
+        }
+
+        public void run() {
+            ChannelFuture future = channel.writeAndFlush("hello");
+            future.addListener(new ChannelFutureListener() {
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    System.out.println("======");
+                    if (future.isSuccess()) {
+                        Thread.sleep(5000);
+                        System.out.println("======isSuccess");
+                    }
+                }
+            });
+
+            System.out.println("继续执行，不阻塞");
+        }
     }
 }
