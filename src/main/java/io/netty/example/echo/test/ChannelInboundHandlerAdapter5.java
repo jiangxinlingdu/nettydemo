4ed6f63232539271d2b805e0d5c758702d62809b
lirenzuo <97019752@qq.om>
2018-12-04 22:14:30 +0800

    m

--- src/main/java/io/netty/example/echo/test/ChannelInboundHandlerAdapter5.java
+++ src/main/java/io/netty/example/echo/test/ChannelInboundHandlerAdapter5.java
@@ -1,6 +1,5 @@
 package io.netty.example.echo.test;
 
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -11,7 +10,7 @@ public class ChannelInboundHandlerAdapter5 extends ChannelInboundHandlerAdapter 
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("ChannelInboundHandlerAdapter5");
 
-        //这段代码 怎么执行都是继续执行，不阻塞 最后，ractor线程里面都是同步的
+        //这段代码 怎么执行都是顺序执行，不阻塞 最后，ractor线程里面都是同步的
         ChannelFuture future = ctx.channel().writeAndFlush("hello");
         future.addListener(new ChannelFutureListener() {
             public void operationComplete(ChannelFuture future) throws Exception {
