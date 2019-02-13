2f0a48f941e0d54c3984b56de47883c0c8bdd379
renzuoli <xxxxxxxxxxxxxx>
2019-02-13 10:01:43 +0800

    m

--- src/main/java/io/netty/example/echo/test/ByteBufTest.java
+++ src/main/java/io/netty/example/echo/test/ByteBufTest.java
@@ -8,5 +8,9 @@ public class ByteBufTest {
     public static void main(String[] args) {
         ByteBufAllocator  alloc = PooledByteBufAllocator.DEFAULT;
         ByteBuf byteBuf = alloc.directBuffer(8*1024);
+
+
+        //很重要，内存释放
+        byteBuf.release();
     }
 }
