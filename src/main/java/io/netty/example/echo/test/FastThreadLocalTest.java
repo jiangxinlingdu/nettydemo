1b30b7c2c337e604d7a65081ae6ef7ed3a694ed9
jiangxinlingdu <xxxxxxxxxxxxxx>
2019-04-24 15:14:15 +0800

    m

--- src/main/java/io/netty/example/echo/test/FastThreadLocalTest.java
+++ src/main/java/io/netty/example/echo/test/FastThreadLocalTest.java
@@ -6,6 +6,29 @@ import io.netty.util.concurrent.FastThreadLocalThread;
 
 public class FastThreadLocalTest {
     private static FastThreadLocal<Integer> fastThreadLocal = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal1 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal2 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal3 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal4 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal5 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal6 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal7 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal8 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal9 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal19 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal29 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal28 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal27 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal26 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal25 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal24 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal23 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal22 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal21 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal20 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal39 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal79 = new FastThreadLocal<Integer>();
+    private static FastThreadLocal<Integer> fastThreadLocal99 = new FastThreadLocal<Integer>();
 
     public static void main(String[] args) {
 
@@ -14,8 +37,8 @@ public class FastThreadLocalTest {
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
-                    fastThreadLocal.set(i);
-                    System.out.println(Thread.currentThread().getName() + "====" + fastThreadLocal.get());
+                    fastThreadLocal99.set(i);
+                    System.out.println(Thread.currentThread().getName() + "====" + fastThreadLocal99.get());
                     try {
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
@@ -30,7 +53,7 @@ public class FastThreadLocalTest {
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
-                    System.out.println(Thread.currentThread().getName() + "====" + fastThreadLocal.get());
+                    System.out.println(Thread.currentThread().getName() + "====" + fastThreadLocal99.get());
                     try {
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
