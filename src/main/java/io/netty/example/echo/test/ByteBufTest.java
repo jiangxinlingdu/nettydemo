40b128444bd4190aabbce1931d873dab8958988a
jiangxinlingdu <xxxxxxxxxxxxxx>
2019-04-28 09:54:34 +0800

    m

--- src/main/java/io/netty/example/echo/test/ByteBufTest.java
+++ src/main/java/io/netty/example/echo/test/ByteBufTest.java
@@ -8,6 +8,8 @@ import io.netty.buffer.Unpooled;
 public class ByteBufTest {
     public static void main(String[] args) {
         ByteBufAllocator alloc = PooledByteBufAllocator.DEFAULT;
+
+        //tiny规格内存分配
         ByteBuf byteBuf = alloc.directBuffer(256);
 
         byteBuf.writeInt(126);
@@ -15,6 +17,15 @@ public class ByteBufTest {
         //很重要，内存释放
         byteBuf.release();
 
+        //走缓存
+        byteBuf = alloc.directBuffer(256);
+
+        byteBuf.writeInt(126);
+        System.out.println(byteBuf.readInt());
+        //很重要，内存释放
+        byteBuf.release();
+
+        //small规格内存分配
         byteBuf = alloc.directBuffer(2 * 1024);
 
         byteBuf.writeInt(127);
@@ -22,12 +33,27 @@ public class ByteBufTest {
         //很重要，内存释放
         byteBuf.release();
 
+        //走缓存
+        byteBuf = alloc.directBuffer(2 * 1024);
+
+        byteBuf.writeInt(127);
+        System.out.println(byteBuf.readInt());
+        //很重要，内存释放
+        byteBuf.release();
+
+        //normal规格内存分配
         byteBuf = alloc.directBuffer(8 * 1024);
         byteBuf.writeInt(128);
         System.out.println(byteBuf.readInt());
         //很重要，内存释放
         byteBuf.release();
 
+        //走缓存
+        byteBuf = alloc.directBuffer(8 * 1024);
+        byteBuf.writeInt(128);
+        System.out.println(byteBuf.readInt());
+        //很重要，内存释放
+        byteBuf.release();
 
         ByteBuf byteBuf1 = Unpooled.buffer(10);
         byteBuf1.release();
