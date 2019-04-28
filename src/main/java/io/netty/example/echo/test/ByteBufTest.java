718987bbefc8e5f45207a3b8d25b7a66199dc0d9
jiangxinlingdu <xxxxxxxxxxxxxx>
2019-04-28 09:42:35 +0800

    m

--- src/main/java/io/netty/example/echo/test/ByteBufTest.java
+++ src/main/java/io/netty/example/echo/test/ByteBufTest.java
@@ -8,7 +8,7 @@ import io.netty.buffer.Unpooled;
 public class ByteBufTest {
     public static void main(String[] args) {
         ByteBufAllocator alloc = PooledByteBufAllocator.DEFAULT;
-        ByteBuf byteBuf = alloc.directBuffer(8 * 1024);
+        ByteBuf byteBuf = alloc.directBuffer(256);
 
         byteBuf.writeInt(126);
         System.out.println(byteBuf.readInt());
@@ -20,11 +20,13 @@ public class ByteBufTest {
         byteBuf.writeInt(127);
         System.out.println(byteBuf.readInt());
         //很重要，内存释放
+        byteBuf.release();
 
-        byteBuf = alloc.directBuffer(256);
+        byteBuf = alloc.directBuffer(8 * 1024);
         byteBuf.writeInt(128);
         System.out.println(byteBuf.readInt());
         //很重要，内存释放
+        byteBuf.release();
 
 
         ByteBuf byteBuf1 = Unpooled.buffer(10);
