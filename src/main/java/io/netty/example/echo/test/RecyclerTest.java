0d29bf3020abb5ff905d971e730d1c0e60c9a3a6
jiangxinlingdu <xxxxxxxxxxxxxx>
2019-05-05 09:30:30 +0800

    m

--- src/main/java/io/netty/example/echo/test/RecyclerTest.java
+++ src/main/java/io/netty/example/echo/test/RecyclerTest.java
@@ -17,11 +17,22 @@ public class RecyclerTest {
 
 
     public static void main(String[] args) {
+
+        try {
+            differentThreadRecycler();
+        } catch (InterruptedException e) {
+        }
+
+        sameThreadRecycler();
+    }
+
+    private static void sameThreadRecycler() {
         User user = recycler.get();
         System.out.println(user.getName());
 
         user.setName("匠心零度");
 
+        //当前回收的线程和Stack所属的线程一致
         user.recycle();
 
 
@@ -32,6 +43,7 @@ public class RecyclerTest {
 
             user2.setName("匠心-零度");
 
+            //当前回收的线程和Stack所属的线程一致
             user2.recycle();
 
             user2 = recycler.get();
@@ -43,6 +55,31 @@ public class RecyclerTest {
         user = recycler.get();
         System.out.println(Thread.currentThread().getName() + ":" + user.getName());
     }
+
+
+    public static void differentThreadRecycler() throws InterruptedException {
+        //Stack属于main线程
+        User user1 = recycler.get();
+        System.out.println(user1.getName());
+        user1.setName("hello,java");
+
+        Thread thread = new Thread(() -> {
+            System.out.println(user1.getName());
+            user1.setName("hello,netty");
+            //当前回收的线程和Stack所属的线程不一致 需要添加到WeakOrderQueue中
+            user1.recycle();
+        });
+
+        thread.start();
+        thread.join();
+
+        // 将其他线程回收的对象重新加入到Stack中进行获取
+        User user2 = recycler.get();
+        System.out.println(user2.getName());
+        //等于true 表示是同一对象
+        System.out.println(user1 == user2);
+    }
+
 }
 
 class User {
