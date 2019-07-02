940e4988d23a83c43c83c32e6ec7de79234f58ac
jiangxinlingdu <xxxxxxxxxxxxxx>
2019-07-02 16:11:59 +0800

    m

--- src/main/java/io/netty/example/echo/test/ValuePaddingTest.java
+++ src/main/java/io/netty/example/echo/test/ValuePaddingTest.java
@@ -51,7 +51,7 @@ public class ValuePaddingTest implements Runnable {
         protected long p1, p2, p3, p4, p5, p6, p7;
         protected volatile long value = 0L;
         protected long p9, p10, p11, p12, p13, p14;
-        protected int p15;
+        protected long p15;
     }
 
     public final static class ValueNoPadding {
